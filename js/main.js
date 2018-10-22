bbd9d839377d75123ba1770215f80564a382669a
Suraj_Lab <dadralj18@gmail.com>
2018-10-22 14:42:05 +0530

    made links accessible

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     setTimeout(function(){
         $('body').addClass('loaded');
         $('.entry-header').hide();
+	$('#loader-wrapper').hide();
     }, 3000);
  
 });
